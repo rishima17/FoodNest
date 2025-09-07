14a1389a93182a6b819d374f89e27ef6874ced14
Rishima <rishima1711@gmail.com>
2025-09-07 23:45:31 +0530

    Update Navbar.jsx

--- admin/src/components/navbar/Navbar.jsx
+++ admin/src/components/navbar/Navbar.jsx
@@ -1,9 +1,9 @@
 import React from "react";
-import "./navbar.css";
+import "./Navbar.css";
 
 import { assets } from "../../assets/assets";
 
-const navbar = () => {
+const Navbar = () => {
   return (
     <div className="navbar">
       <img className="logo " src={assets.logo} alt="" />
