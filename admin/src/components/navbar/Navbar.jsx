541261d6f4f6be633888d0af28882aabf61796a5
Rishima <rishima1711@gmail.com>
2025-09-11 17:40:20 +0530

    Update Navbar.jsx

--- admin/src/components/navbar/Navbar.jsx
+++ admin/src/components/navbar/Navbar.jsx
@@ -6,7 +6,7 @@ import { assets } from "../../assets/assets";
 const Navbar = () => {
   return (
     <div className="navbar">
-      <img className="logo " src={assets.logo} alt="" />
+      <img className="logo " src={assets.Logo} alt="" />
       <img className="profile" src={assets.profile_image} alt="" />
     </div>
   );
